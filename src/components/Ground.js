d15ea6da38410d6f95d51146b0ebd23382cffceb
j@n <jan_spam@posteo.de>
2023-04-19 10:26:02 +0200

    ground added, rotated an filled with groundTexture

---
+++ src/components/Ground.js
@@ -0,0 +1,27 @@
+import { usePlane } from "@react-three/cannon";
+import { groundTexture } from '../images/textures';
+import { NearestFilter, RepeatWrapping } from "three";
+
+export const Ground = () => {
+
+    // add a plane ground to the canvas
+    const [ref] = usePlane(() => ({
+        // the ground is standing upwards --> solution: it has to be rotated by Pi/4 degree radian (45°)
+        rotation: [-Math.PI/4, 0, 0], position: [0, 0, 0]
+    }));
+
+    // the ground looks blury --> solution: use a filter, so that it looks pixelated
+    groundTexture.magFilter = NearestFilter;
+
+    // the ground is jetzt a ground square, the image got streched --> solution: repeat the ground texture
+    groundTexture.wrapS = RepeatWrapping;
+    groundTexture.wrapT = RepeatWrapping;
+    groundTexture.repeat.set(100, 100);
+
+    return (
+        <mesh ref={ ref }>
+            <planeBufferGeometry attach='geometry' args={[100, 100]} />
+            <meshStandardMaterial attach='material' map={groundTexture} />
+        </mesh>
+    );
+}
