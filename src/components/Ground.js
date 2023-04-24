fe79edc11d32740c4fb11d37770b37f6e3291b70
j@n <jan_spam@posteo.de>
2023-04-24 17:24:02 +0200

    it is now possible to add and remove one cube at a time

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,18 +1,28 @@
 import { usePlane } from "@react-three/cannon";
 import { groundTexture } from '../images/textures';
+import { useStore } from "../hooks/useStore";
 
 export const Ground = () => {
 
     // add a plane ground to the canvas
     const [ref] = usePlane(() => ({
         // the ground is standing upwards --> solution: it has to be rotated by Pi/2 degree radian (90°)
-        rotation: [-Math.PI/2, 0, 0], position: [0, 0, 0]
+        rotation: [-Math.PI/2, 0, 0], position: [0, -0.5, 0]
     }));
+
+    const [addCube] = useStore((state) => [state.addCube]);
 
     groundTexture.repeat.set(100, 100);
 
     return (
-        <mesh ref={ ref }>
+        <mesh
+        onClick={(e) => {
+            e.stopPropagation();
+            const [x, y, z] = Object.values(e.point).map(val => Math.ceil(val));
+            addCube(x, y, z);
+        }} 
+        ref={ ref }
+        >
             <planeBufferGeometry attach='geometry' args={[100, 100]} />
             <meshStandardMaterial attach='material' map={groundTexture} />
         </mesh>
